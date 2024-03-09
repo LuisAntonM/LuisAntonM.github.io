e0c39b46d0f39d2c179ae8454ede3d819ace6beb
LuisAntonM <162662271+LuisAntonM@users.noreply.github.com>
2024-03-08 21:31:07 -0700

    Add files via upload

---
+++ Ejercicio_Javascript/scripts/ejercicio.js
@@ -0,0 +1,12 @@
+
+function cambiarColor(){
+    variable_album=document.getElementById("album");
+    console.log(variable_album);
+    variable_album.style.backgroundColor="yellow";
+}
+function cambiarTamano(){
+    variable_clase=document.getElementsByClassName("imagen")[0];
+    variable_clase.style.height="500px";
+    variable_clase.style.width="Auto";
+
+}
